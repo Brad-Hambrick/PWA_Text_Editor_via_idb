d69ab00ef9899a7c472494a57391a03cc07b41b9
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2022-12-13 11:42:55 -0600

    added code to webpack config, service worker, and javascript to put and get from db as well as install.  Also added scripts for building, installing, and running the app

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log("Add to the datbase");
+  // connect to DB
+  const connectDb = await openDB("jate", 1);
+  // Create new transaction
+  const tx = connectDb.transaction("jate", "readwrite");
+  // Open object store
+  const store = tx.objectStore("jate");
+  // Pass content into the store
+  const request = store.put({ Text: content });
+  // confirm content was added to the store
+  const result = await request;
+  console.log("Successfully saved to the database", result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.log("Get from the database");
+  // Connect to DB
+  const connectDb = await openDB("jate", 1);
+  //Create new transaction
+  const tx = connectDb.transaction("jate", "readonly");
+  // Open object store
+  const store = tx.objectStore("jate");
+  // Get data from DB
+  const request = store.getAll();
+  // Confirm data was retrieved
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 initdb();
